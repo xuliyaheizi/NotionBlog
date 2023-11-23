bd9a313c10ec4a659974fa5900fa87f5092fdde9
ZhuLin <2897669115@qq.com>
2023-11-23 10:19:29 +0800

    zl，feat：优化配置文件参数

--- themes/heo/components/NavButtonGroup.js
+++ themes/heo/components/NavButtonGroup.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-
-import React from 'react'
-=======
 import Link from 'next/link'
->>>>>>> main
 
 /**
  * 首页导航大按钮组件
@@ -18,9 +13,6 @@ const NavButtonGroup = (props) => {
 
   return (
     <nav id='home-nav-button' className={'w-full z-10 md:h-72 md:mt-6 xl:mt-32 px-5 py-2 mt-8 flex flex-wrap md:max-w-5xl space-y-2 md:space-y-1 md:flex justify-center max-h-80 overflow-auto'}>
-<<<<<<< HEAD
-      {}
-=======
       {categoryOptions?.map(category => {
         return (
           <Link
@@ -29,11 +21,10 @@ const NavButtonGroup = (props) => {
             href={`/category/${category.name}`}
             passHref
             className='text-center w-full sm:w-4/5 md:mx-6 md:w-40 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-white hover:text-black duration-200 font-bold hover:scale-105 transform'>
-                {category.name}
-            </Link>
+            {category.name}
+          </Link>
         )
       })}
->>>>>>> main
     </nav>
   )
 }
